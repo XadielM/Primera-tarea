b9ef143085df6d655ae7cd15649d754bad31664d
Xadiel Martinez <20220141@itla.edu.do>
2023-06-16 13:31:19 -0400

    Añadiendo la tarea del capitulo 6 y 7

---
+++ Cap 6 y 7/Ejercicios del capitulo 6/Cap 6/bb.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Carla Manuela\n\n");
+    return 0;
+
+}
---
+++ Cap 6 y 7/Ejercicios del capitulo 7/Ejercicio 7.18.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* Cadena invertida resuelta en forma recursiva. */
+void inverso(char *); 
+int main(void)
+{
+char fra[50];
+printf("\nIngrese la linea de texto: ");
+gets(fra);
+printf("\nEscribe la linea de texto en forma inversa: ");
+inverso(fra);
+}
+void inverso(char *cadena)
+/* La función inverso obtiene precisamente el inverso de la cadena.*/
+{
+if (cadena[0]!= '\0')
+{
+inverso(&cadena[1]);
+putchar(cadena[0]);
+}
+}
